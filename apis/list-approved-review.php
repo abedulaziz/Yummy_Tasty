03d7e495a012d024de2b047e91289d327ddfefeb
MohammadMancy <mohammad.mancy994@gmail.com>
2022-05-31 14:59:03 +0300

    Adding the full name of user had an approved review

--- apis/list-approved-review.php
+++ apis/list-approved-review.php
@@ -3,7 +3,7 @@
 include("connection.php");
 $restaurant_id = $_POST["restaurant_id"];
 
-$query = $mysqli->prepare("SELECT description,rate from reviews where status='approved'and restaurant_id = $restaurant_id");
+$query = $mysqli->prepare("SELECT users.first_name,users.last_name,reviews.description,reviews.rate from reviews,users where status='approved'and restaurant_id = $restaurant_id and users.user_id = reviews.user_id");
 $query->execute();
 $array = $query->get_result();
 
